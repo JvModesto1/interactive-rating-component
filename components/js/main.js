f5b92bfa6b0ea93e448f028115d1a31dd13529f1
João Victor <joaojoovtr@gmail.com>
2022-09-14 23:11:39 -0300

    create the component and add the java script for event listener

---
+++ components/js/main.js
@@ -0,0 +1,36 @@
+const number = document.querySelectorAll('.number');
+const defaultRatingIndex = 0;
+let currentRatingIndex = 0;
+
+
+const setRating = (index) => {
+    number.forEach(number => number.classList.remove('selected'));
+    if (index > 0 && index <= number.length) {
+        document.querySelector(`[data-rate="${index}"]`).classList.add('selected');
+    }
+}
+
+const checkSelectedNumber = (number) => {
+    if (parseInt(number.getAttribute("data-rate")) === currentRatingIndex) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+const resetRating = () => {
+    currentRatingIndex = defaultRatingIndex;
+    setRating(defaultRatingIndex);
+}
+
+number.forEach((number) => {
+    number.addEventListener('click', () => {
+        if  (checkSelectedNumber(number)) {
+            resetRating();
+            return;
+        }
+        const index = parseInt(number.getAttribute("data-rate"));
+        currentRatingIndex = index;
+        setRating(index);
+    });
+}) 
